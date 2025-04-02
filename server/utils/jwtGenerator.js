d4e30cab436400b621323e8d73a0e6416ca2a178
Firomsa Guteta <firomsaguteta12@gmail.com>
2025-04-02 09:01:12 +0300

    feat: Implement JWT generator for user authentication

---
+++ server/utils/jwtGenerator.js
@@ -0,0 +1,19 @@
+import jsonWebToken from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+
+const jwtGenerator = (userId) => {
+  if (!userId) {
+    console.error("Error: user_id is undefined or null");
+    return;
+  }
+  const payload = { userId };
+  return jsonWebToken.sign(
+    payload,
+    process.env.JWT_SECRET,
+    { expiresIn: "1d" } // Token expires in 1 day
+  );
+};
+
+console.log(process.env.JWT_SECRET);
+export default jwtGenerator;
