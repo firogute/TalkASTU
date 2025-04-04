683490532d0dab3b50567f1f521f1e17b75c44a2
Firomsa Guteta <firomsaguteta12@gmail.com>
2025-04-04 17:08:02 +0300

    feat: implement clear functionality for username input and toggle visibility for password fields

--- client/pages/Register.jsx
+++ client/pages/Register.jsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import { FaRegUser, FaLock, FaEye } from "react-icons/fa";
 import { FaXmark } from "react-icons/fa6";
+import { GrView, GrFormViewHide } from "react-icons/gr";
 
 const Register = () => {
+  const [isView, setIsView] = useState(false);
+  const [isConfirmView, setIsConfirmView] = useState(false);
+  const [userName, setUserName] = useState("");
+
   return (
     <div className="w-screen h-screen bg-gradient-to-br from-emerald-50 to-rose-50 flex justify-center items-center p-4">
-      <div className="shadow-xl max-w-md w-full rounded-2xl border border-gray-100 bg-white/80 backdrop-blur-sm p-8">
+      <div className="shadow-xl max-w-md w-full rounded-2xl border border-gray-100 bg-white/80 backdrop-blur-sm p-8 flex flex-col">
         <div className="flex flex-col items-center mb-8">
           <h1 className="text-3xl font-bold text-gray-800 mb-2">Register</h1>
           <p className="text-lg text-gray-600">
@@ -32,12 +38,17 @@ const Register = () => {
                   type="text"
                   id="uname"
                   name="uname"
+                  value={userName}
                   placeholder="Enter your username"
                   className="block w-full pl-10 pr-3 py-3 rounded-xl border border-gray-300 focus:ring-2 focus:ring-emerald-500 focus:border-emerald-500 bg-white/90"
                   minLength="8"
+                  onChange={(e) => setUserName(e.target.value)}
                   required
                 />
-                <div className="absolute inset-y-0 right-0 mr-3 h-fit my-auto p-2 flex items-center hover:bg-gray-300 rounded-full cursor-pointer">
+                <div
+                  className="absolute inset-y-0 right-0 mr-3 h-fit my-auto p-2 flex items-center hover:bg-gray-300 rounded-full cursor-pointer"
+                  onClick={() => setUserName("")}
+                >
                   <FaXmark className="text-gray-900" />
                 </div>
               </div>
@@ -55,7 +66,7 @@ const Register = () => {
                   <FaLock className="text-gray-400" />
                 </div>
                 <input
-                  type="password"
+                  type={isView ? "text" : "password"}
                   name="password"
                   id="password"
                   placeholder="Enter your password"
@@ -63,8 +74,15 @@ const Register = () => {
                   className="block w-full pl-10 pr-3 py-3 rounded-xl border border-gray-300 focus:ring-2 focus:ring-emerald-500 focus:border-emerald-500 bg-white/90"
                   required
                 />
-                <div className="absolute inset-y-0 right-0 mr-3 h-fit my-auto p-2 flex items-center hover:bg-gray-300 rounded-full cursor-pointer">
-                  <FaEye className="text-gray-900" />
+                <div
+                  className="absolute inset-y-0 right-0 mr-3 h-fit my-auto p-2 flex items-center hover:bg-gray-300 rounded-full cursor-pointer"
+                  onClick={() => setIsView(!isView)}
+                >
+                  {isView ? (
+                    <GrView className="text-gray-900" />
+                  ) : (
+                    <GrFormViewHide className="text-gray-900" />
+                  )}
                 </div>
               </div>
             </div>
@@ -81,15 +99,22 @@ const Register = () => {
                   <FaLock className="text-gray-400" />
                 </div>
                 <input
-                  type="password"
+                  type={isConfirmView ? "text" : "password"}
                   name="confirmPassword"
                   id="confirmPassword"
                   placeholder="Confirm Password"
                   className="block w-full pl-10 pr-3 py-3 rounded-xl border border-gray-300 focus:ring-2 focus:ring-emerald-500 focus:border-emerald-500 bg-white/90"
                   required
                 />
-                <div className="absolute inset-y-0 right-0 mr-3 h-fit my-auto p-2 flex items-center hover:bg-gray-300 rounded-full cursor-pointer">
-                  <FaEye className="text-gray-900" />
+                <div
+                  className="absolute inset-y-0 right-0 mr-3 h-fit my-auto p-2 flex items-center hover:bg-gray-300 rounded-full cursor-pointer"
+                  onClick={() => setIsConfirmView(!isConfirmView)}
+                >
+                  {isConfirmView ? (
+                    <GrView className="text-gray-900" />
+                  ) : (
+                    <GrFormViewHide className="text-gray-900" />
+                  )}
                 </div>
               </div>
             </div>
@@ -102,6 +127,14 @@ const Register = () => {
             Register
           </button>
         </form>
+        <div className="self-center my-5 text-[17px]">
+          <p>
+            Already Have an account?
+            <Link to="/login" className="text-emerald-800 underline p-1">
+              Login
+            </Link>
+          </p>
+        </div>
       </div>
     </div>
   );
